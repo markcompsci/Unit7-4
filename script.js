253ff48f0547d6eaad7ccc8b8fd12908c0a4d3ff
markcompsci <84529187+markcompsci@users.noreply.github.com>
2021-06-09 16:50:58 +0000

    Made calculator buttonless!

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ const CALC = document.getElementById('calc-btn')
 const MSG = document.createElement('p')
 document.body.appendChild(MSG)
 
-CALC.addEventListener('click', () => {
+// This would have been used with the button
+
+// CALC.addEventListener('click', () => {
+//   if ((DAY.value === 'Tuesday' || DAY.value === 'Thursday') || (AGE.value > 12 && AGE.value < 21)) {
+//     MSG.textContent = 'You qualify for a discount!'
+//   } else {
+//     MSG.textContent = 'There aren\'t any discounts.'
+//   }
+// })
+
+// Newly made function that is called when you click out of the div that holds the two fields. 
+// It will check if the day is Tuesday or Thursday, if so, you get a discount
+// Or, if your age is in between 12 and 21 you will also get a discount
+// Otherwise, no discount
+function calculateDiscount () {
   if ((DAY.value === 'Tuesday' || DAY.value === 'Thursday') || (AGE.value > 12 && AGE.value < 21)) {
     MSG.textContent = 'You qualify for a discount!'
   } else {
     MSG.textContent = 'There aren\'t any discounts.'
   }
-})
+}
